9a42d02cc9e936fa8deede6b986668d1f1a6d9d3
Андрей Богидаев <andrey.bogidaev03@gmail.com>
2023-07-13 12:07:09 +0300

    Добавление таблицы Group

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
 
 class Post(models.Model):
     text = models.TextField()
@@ -11,6 +18,13 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        related_name="posts",
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return self.text
